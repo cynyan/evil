cynyan/evil

commit 8b4fb18216d0e112fb1692301fcfe0128aa08852

Force writing of file with ":w!"
By default, Emacs only saves to disk when there are unsaved changes.
This can be overridden with "!".

evil-commands.el
@@ -1994,29 +1994,26 @@ See also `evil-open-fold'."
 
 ;;; Ex
 
-(evil-define-operator evil-write (beg end type file-name &optional bang)
-  "Save the current buffer, from BEG to END, to FILE-NAME.
-If the BANG argument is non-nil, the file will be overwritten
-if already existing. Does not change the current buffer's name."
-  :motion mark-whole-buffer
+(evil-define-operator evil-write (beg end type filename &optional bang)
+  "Save the current buffer, from BEG to END, to FILENAME.
+If the file already exists and the BANG argument is non-nil,
+it is overwritten without confirmation."
+  :motion nil
   :move-point nil
   :type line
   :repeat nil
   (interactive "<R><f><!>")
-  (when (null file-name)
-    (setq file-name (buffer-file-name))
-    (unless file-name
-      (error "Please specify a file-name for this buffer!")))
-
-  (cond
-   ((and (= beg (point-min)) (= end (point-max))
-         (string= file-name (buffer-file-name)))
-    (save-buffer))
-   ((and (= beg (point-min)) (= end (point-max))
-         (null (buffer-file-name)))
-    (write-file file-name (not bang)))
-   (t
-    (write-region beg end file-name nil nil nil (not bang)))))
+  (when (zerop (length filename))
+    (setq filename (buffer-file-name)))
+  (when (zerop (length filename))
+    (error "Please specify a file name for the buffer"))
+  (if (and beg end)
+      (write-region beg end filename nil nil nil (not bang))
+    (when bang
+      (set-buffer-modified-p t))
+    (if (string= filename (or (buffer-file-name) ""))
+        (save-buffer)
+      (write-file filename (not bang)))))
 
 (evil-define-command evil-write-all (bang)
   "Saves all buffers."
@@ -2035,7 +2032,7 @@ If no FILE is given, the current file name is used."
   (unless file
     (setq file (buffer-file-name))
     (unless file
-      (error "Please specify a file name for this buffer!")))
+      (error "Please specify a file name for the buffer")))
   (write-file file (not bang)))
 
 (evil-define-command evil-edit (file &optional bang)

commit 3f878181d666655f2c7ba0db8b3191a619f1ae9e

Make `evil-write' *not* change the current buffer's file name
According to Vim, the :w {file} command does not change the current
buffer's file name unless the current buffer is not visiting a file.
In contrast, the :saveas command changes the file name of the current
buffer. This reverts a bug introduced in 8b4fb182.

evil-commands.el
@@ -2055,8 +2055,10 @@ See also `evil-open-fold'."
 
 (evil-define-operator evil-write (beg end type filename &optional bang)
   "Save the current buffer, from BEG to END, to FILENAME.
-If the file already exists and the BANG argument is non-nil,
-it is overwritten without confirmation."
+The current buffer's filename is not changed unless it has no
+associated file and no region is specified.  If the file already
+exists and the BANG argument is non-nil, it is overwritten
+without confirmation."
   :motion nil
   :move-point nil
   :type line
@@ -2067,12 +2069,17 @@ it is overwritten without confirmation."
   (cond
    ((zerop (length filename))
     (error "Please specify a file name for the buffer"))
+   ;; with region always save to file without reseting modified flag
    ((and beg end)
     (write-region beg end filename nil nil nil (not bang)))
-   ((and (not bang) (string= filename (or (buffer-file-name) "")))
-    (save-buffer))
+   ;; save current buffer to its file
+   ((string= filename (or (buffer-file-name) ""))
+    (if (not bang) (save-buffer) (write-file filename)))
+   ;; save to other file
    (t
-    (write-file filename (not bang)))))
+    (write-region nil nil filename
+                  nil (not (buffer-file-name)) nil
+                  (not bang)))))
 
 (evil-define-command evil-write-all (bang)
   "Saves all buffers."
@@ -2081,14 +2088,16 @@ it is overwritten without confirmation."
   (interactive "<!>")
   (save-some-buffers bang))
 
-(evil-define-command evil-save (file &optional bang)
-  "Save the current buffer to FILE.
-Changes the file name of the current buffer to this name.
-If no FILE is given, the current file name is used."
+(evil-define-command evil-save (filename &optional bang)
+  "Save the current buffer to FILENAME.
+Changes the file name of the current buffer to FILENAME.  If no
+FILENAME is given, the current file name is used."
   :repeat nil
   :move-point nil
   (interactive "<f><!>")
-  (evil-write nil nil nil file bang))
+  (when (zerop (length filename))
+    (setq filename (buffer-file-name)))
+  (write-file filename (not bang)))
 
 (evil-define-command evil-edit (file &optional bang)
   "Open FILE.