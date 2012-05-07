cynyan/evil

commit 69dfdcbd52eb84f3037f6af45c18551f980bd751

Make `evil-apply-on-block' respect extension of block to eol
When the last command has been a line-motion and the goal column is
very large then the function is applied on each line up to the end of
that line. This addresses issue #120.

evil-common.el
@@ -1576,7 +1576,10 @@ Then restore Transient Mark mode to its previous setting."
 The selection may be specified explicitly with BEG and END.
 FUNC must take at least two arguments, the beginning and end of
 each line. Extra arguments to FUNC may be passed via ARGS."
-  (let (beg-marker end-marker left right eob)
+  (let ((eol-col (and (memq last-command '(next-line previous-line))
+                      (numberp temporary-goal-column)
+                      temporary-goal-column))
+        beg-marker end-marker left right eob)
     (save-excursion
       (evil-sort beg end)
       ;; calculate columns
@@ -1605,7 +1608,7 @@ each line. Extra arguments to FUNC may be passed via ARGS."
                         (move-to-column left t)
                         (point))
                       (save-excursion
-                        (move-to-column right t)
+                        (move-to-column (or eol-col right))
                         (point))
                       args)
                (forward-line 1)

commit 7b2712e3cb46ea0303d54010f51dce6d4a02aa82

Fix `evil-apply-on-block' on one-column blocks
Commit 69dfdcbd5 introduced the following bug. If
`temporary-goal-column' has been set to a number not equal to
most-positive-fixnum (e.g., the current column) then the right most
column is replaced by `eol-col' but it should be (1+ eol-col) because
the right end is exclusive. We now only use the value of
`temporary-goal-column' if its value equals `most-positive-fixnum'.

evil-common.el
@@ -1578,6 +1578,7 @@ FUNC must take at least two arguments, the beginning and end of
 each line. Extra arguments to FUNC may be passed via ARGS."
   (let ((eol-col (and (memq last-command '(next-line previous-line))
                       (numberp temporary-goal-column)
+                      (= temporary-goal-column most-positive-fixnum)
                       temporary-goal-column))
         beg-marker end-marker left right eob)
     (save-excursion